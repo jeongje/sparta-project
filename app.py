f330dbb02f9c61b1656fb7a5df78cf9e23939ea4
jeongje <uonnow@gmail.com>
2020-02-23 14:07:02 +0900

    DB생성 tenants테이블 완성

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rent.db'
+db = SQLAlchemy(app)
+
+class Tenants(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    tenant_memo = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        return '<Tenants %r>' % str(self.id)
+
 
 @app.route('/')
 def index():
