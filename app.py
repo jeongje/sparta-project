bdcfefb8985acad8e9cb9f3dbe29ef351b460f99
jeongje <uonnow@gmail.com>
2020-02-23 13:34:46 +0900

    입주자입력 html 생성

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/tenants')
+def tenants():
+    return render_template('tenants.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
